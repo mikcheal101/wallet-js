b3dd0518e1634229cd023fd0da1ccc48b2928a55
mikcheal101 <hirekaanmicheal@gmail.com>
2024-05-31 16:08:51 +0100

    main defined

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import express, { Express } from "express";
+import accountRoutes from "./routes/account.route";
+
+const app:Express = express();
+const port:number = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// routes file
+app.use("/api/v1/accounts", accountRoutes);
+
+app.listen(port, () => {
+    console.log(`listening on http:\\localhost:${port}`);
+});
+
